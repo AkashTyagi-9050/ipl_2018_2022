0a9b868c0120c71b633584e30b10e7065bb72b2e
akash <akashgal2019@gmail.com>
2024-05-17 23:14:33 +0530

    added sidebar

--- IPL.py
+++ IPL.py
@@ -6,5 +6,11 @@ import seaborn as sns
 
 st.title('IPL 2018-2022')
 
+st.sidebar.title('Choose Category here....!')
+
+st.sidebar.selectbox('Batting',['Most Run', 'Highest Scores', 'Best Batting Average', 'Best Batting Strike Rate', 'Most Hundreds', 'Most Fities', 'Mosst Fours', 'Most Sixes'])
+st.sidebar.selectbox('Bowling',['Most Wickets', 'Best Bowling Average', 'Best Bowling', 'Most 5 Wickets Haul', 'Best Economy', 'Best Bowling Strike Rate'])
+
+
 
 
